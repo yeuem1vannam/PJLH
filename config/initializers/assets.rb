3b2003ddb6dbdfda4eb64c6afd0968fc4c030107
Phương 'J' Lê H <phuonglh0420@gmail.com>
2014-09-05 05:39:09 +0000

    #3

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,2 @@
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
 Rails.application.config.assets.precompile += %w( style.css style-wide.css style-normal.css style-narrow.css style-mobile.css font-awesome.min.css )
